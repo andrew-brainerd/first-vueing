6e00f80dd0188435852684d6b6fdef7b22b8d327
Andrew Brainerd <drwb333@gmail.com>
2020-07-21 21:04:02 -0400

    Add toggle dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :style="{ background: darkMode ? 'black' : '#666666' }">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld />
+    <button @click="toggleDarkMode">{{ darkMode ? 'Light' : 'Dark'}} Mode</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      darkMode: true
+    };
+  },
+  methods: {
+    toggleDarkMode() {
+      this.darkMode = !this.darkMode
+    }
   }
-}
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  width: 100%;
 }
 </style>
